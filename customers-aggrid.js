bdbb8522a07126049bd03e3c3647d7fd7671f49b
madhumorigadi <madhumorigadi@gmail.com>
2022-05-27 13:54:30 +0530

    working on updating d

--- customers-aggrid.js
+++ customers-aggrid.js
@@ -1,50 +1,36 @@
-var __decorate =
-  (this && this.__decorate) ||
-  function (decorators, target, key, desc) {
-    var c = arguments.length,
-      r =
-        c < 3
-          ? target
-          : desc === null
-          ? (desc = Object.getOwnPropertyDescriptor(target, key))
-          : desc,
-      d;
-    if (typeof Reflect === 'object' && typeof Reflect.decorate === 'function')
-      r = Reflect.decorate(decorators, target, key, desc);
-    else
-      for (var i = decorators.length - 1; i >= 0; i--)
-        if ((d = decorators[i]))
-          r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
-  };
-import {html, LitElement} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+};
+import { html, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
 // import {Grid, GridOptions} from '@ag-grid-community/core';
 // import {Grid} from 'ag-grid-community';
 // import '../node_modules/ag-grid-community/dist/ag-grid-community';
-
-import 'ag-grid-community/dist/ag-grid-community';
-
+// import 'ag-grid-webcomponent/src/agGrid';
+// import 'ag-grid-community/dist/ag-grid-community';
 let CustomersAggrid = class CustomersAggrid extends LitElement {
-  constructor() {
-    super();
-    this.rowData = [
-      {make: 'Toyota1', model: 'Celica', price: 35000},
-      {make: 'Ford2', model: 'Mondeo', price: 32000},
-      {make: 'Porsche3', model: 'Boxter', price: 72000},
-    ];
-    this.gridOptions = {
-      columnDefs: [
-        {headerName: 'Make', field: 'make'},
-        {headerName: 'Model', field: 'model'},
-        {headerName: 'Price', field: 'price'},
-      ],
-    };
-  }
-  // specify the columns
-  // specify the data
-  render() {
-    return html`
+    constructor() {
+        super();
+        this.rowData = [
+            { make: 'Toyota1', model: 'Celica', price: 35000 },
+            { make: 'Ford2', model: 'Mondeo', price: 32000 },
+            { make: 'Porsche3', model: 'Boxter', price: 72000 },
+        ];
+        this.gridOptions = {
+            columnDefs: [
+                { headerName: 'Make', field: 'make' },
+                { headerName: 'Model', field: 'model' },
+                { headerName: 'Price', field: 'price' },
+            ],
+        };
+    }
+    // specify the columns
+    // specify the data
+    render() {
+        return html `
       <ag-grid
         id="detailsGrid"
         class="ag-theme-dark"
@@ -52,13 +38,16 @@ let CustomersAggrid = class CustomersAggrid extends LitElement {
         .rowData="${this.rowData}"
       ></ag-grid>
     `;
-  }
+    }
 };
-__decorate([property()], CustomersAggrid.prototype, 'rowData', void 0);
-__decorate([property()], CustomersAggrid.prototype, 'gridOptions', void 0);
-CustomersAggrid = __decorate(
-  [customElement('customers-aggrid')],
-  CustomersAggrid
-);
-export {CustomersAggrid};
+__decorate([
+    property()
+], CustomersAggrid.prototype, "rowData", void 0);
+__decorate([
+    property()
+], CustomersAggrid.prototype, "gridOptions", void 0);
+CustomersAggrid = __decorate([
+    customElement('customers-aggrid')
+], CustomersAggrid);
+export { CustomersAggrid };
 //# sourceMappingURL=customers-aggrid.js.map
--- src/customers-aggrid.ts
+++ src/customers-aggrid.ts
@@ -3,15 +3,14 @@ import {customElement, property} from 'lit/decorators.js';
 // import {Grid, GridOptions} from '@ag-grid-community/core';
 // import {Grid} from 'ag-grid-community';
 // import '../node_modules/ag-grid-community/dist/ag-grid-community';
-import '/ag-grid-webcomponent/src/agGrid';
-import 'ag-grid-community/dist/ag-grid-community';
-import '../node_modules/ag-grid-community//main';
-import '../node_modules/ag-grid-webcomponent/src';
+// import 'ag-grid-webcomponent/src/agGrid';
+// import 'ag-grid-community/dist/ag-grid-community';
 
 @customElement('customers-aggrid')
 export class CustomersAggrid extends LitElement {
   constructor() {
     super();
+
     this.rowData = [
       {make: 'Toyota1', model: 'Celica', price: 35000},
       {make: 'Ford2', model: 'Mondeo', price: 32000},
